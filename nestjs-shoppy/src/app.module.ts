45cc455e4448627caa3953c3afeb312c987956d1
Joanderson <sirjoandersonwf@gmail.com>
2024-07-14 20:13:53 -0300

    add: backend

---
+++ nestjs-shoppy/src/app.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
+        return {
+          pinoHttp: {
+            transport: isProduction
+              ? undefined
+              : {
+                  target: 'pino-pretty',
+                  options: {
+                    singleLine: true,
+                  },
+                },
+            level: isProduction ? 'info' : 'debug',
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        PORT: Joi.number().required(),
+        HASH_SALT: Joi.number().required(),
+      }),
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ nestjs-shoppy/src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { CreateUserRequest } from './dto/create-user.request';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async createUser(data: CreateUserRequest) {
+    console.log(data);
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          email: data.email,
+          password: await bcrypt.hash(
+            data.password,
+            this.configService.get<number>('HASH_SALT'),
+          ),
+        },
+        select: { id: true, email: true },
+      });
+    } catch (err) {
+      console.log(err);
+      if (err.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already taken');
+      }
+      throw err;
+    }
+  }
+}
