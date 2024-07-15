f52b7f3a90d48453e7fab3610d30e8c0178f9099
Joanderson <sirjoandersonwf@gmail.com>
2024-07-14 21:41:29 -0300

    feat: fetch wrapper

--- nextjs-shoppyui/app/auth/signup/create-user.ts
+++ nextjs-shoppyui/app/auth/signup/create-user.ts
@@ -3,16 +3,12 @@
 import { redirect } from "next/navigation";
 import { API_URL } from "../constants/api";
 import { getErrorMessage } from "../utils/errors";
+import { post } from "../utils/fetch";
 
 export default async function createUser(_prevState: any, formData: any) {
-  const res = await fetch(`${API_URL}/users`, {
-    method: "POST",
-    body: formData,
-  });
-  const parsedRes = await res.json();
-  if (!res.ok) {
-    return { error: getErrorMessage(parsedRes) };
+  const { error } = await post("users", formData);
+  if (error) {
+    return { error };
   }
-
   redirect("/");
 }
