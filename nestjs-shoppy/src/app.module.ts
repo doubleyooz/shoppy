8652e2c6e672af85b507ce782f13eee4b766f776
Joanderson <sirjoandersonwf@gmail.com>
2024-07-27 08:53:10 -0300

    feat: create product endpoint

--- nestjs-shoppy/src/app.module.ts
+++ nestjs-shoppy/src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -41,6 +42,7 @@ import * as Joi from 'joi';
     }),
     UsersModule,
     AuthModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
